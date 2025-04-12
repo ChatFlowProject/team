440a6a36350d6b8a1be8ec88f7ef792614a62dea
Chanui <jcu011@naver.com>
2025-04-12 22:39:46 +0900

    fix: jar파일 빌드시 오류 수정

--- src/test/java/shop/flowchat/team/TeamApplicationTests.java
+++ src/test/java/shop/flowchat/team/TeamApplicationTests.java
@@ -2,8 +2,10 @@ package shop.flowchat.team;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class TeamApplicationTests {
 
     @Test
