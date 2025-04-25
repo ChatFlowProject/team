24834ef0eb37e11783111958f2c647a60b596abc
Chanui <jcu011@naver.com>
2025-04-26 00:40:30 +0900

    test: 테스트 클래스 EmbeddedKafka 추가

--- src/test/java/shop/flowchat/team/TeamApplicationTests.java
+++ src/test/java/shop/flowchat/team/TeamApplicationTests.java
@@ -2,8 +2,10 @@ package shop.flowchat.team;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 @SpringBootTest
 @ActiveProfiles("dev") // "dev" 프로파일에서만 테스트 코드 실행됨
 class TeamApplicationTests {
